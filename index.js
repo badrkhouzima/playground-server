3a0a0438b6f78b3c1cfe44060199a6566ec1f480
badr <badrhand@gmail.com>
2023-01-28 13:41:52 +0100

    try this tweak

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin:*");
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin:*");
+//   next();
+// });
